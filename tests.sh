83233354ad5aff50fa045012012f3bd42f1ea0fb
Pavol Marko <pavol@namespacelabs.com>
2025-03-04 17:06:07 +0100

    even more output

--- tests.sh
+++ tests.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-max=200
-for (( i=0; i < max; i++ )); do echo "line $i"; done
+max=30
+for (( i=0; i < max; i++ ))
+do
 
-printf "\`abjkdsljfkds\` has been deprecated, please migrate to \`abjkdsljfkds\`\n at abjkdsljfkds(/src/node_modules/somepath:38392:35)\n at Something.SomethingElse (/src/node_modules/someotherpath.js:123:24)\n"
-cat output
+	echo "line $i"
+	printf "\`abjkdsljfkds\` has been deprecated, please migrate to \`abjkdsljfkds\`\n at abjkdsljfkds(/src/node_modules/somepath:38392:35)\n at Something.SomethingElse (/src/node_modules/someotherpath.js:123:24)\n"
+
+	cat output >&2
+
+done
+
 
 
