e903c0e4b2cd0425f0a060ebef26da57af9db1df
Pavol Marko <pavol@namespacelabs.com>
2025-03-04 17:02:27 +0100

    more output

--- tests.sh
+++ tests.sh
@@ -1,2 +1,9 @@
-#!/bin/sh
-echo test
+#!/bin/bash
+
+max=200
+for (( i=0; i < max; i++ )); do echo "line $i"; done
+
+printf "\`abjkdsljfkds\` has been deprecated, please migrate to \`abjkdsljfkds\`\n at abjkdsljfkds(/src/node_modules/somepath:38392:35)\n at Something.SomethingElse (/src/node_modules/someotherpath.js:123:24)\n"
+cat output
+
+
