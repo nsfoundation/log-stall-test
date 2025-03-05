18b3aecb488c3e4822f0e41deb3aed057a505836
Pavol Marko <pavol@namespacelabs.com>
2025-03-05 10:44:58 +0100

    stderr?

--- testwithdocker/tests.sh
+++ testwithdocker/tests.sh
@@ -9,6 +9,12 @@ do
 	printf "\`abjkdsljfkds\` has been deprecated, please migrate to \`abjkdsljfkds\` at abjkdsljfkds(/src/node_modules/somepath:38392:35) at Something.SomethingElse (/src/node_modules/someotherpath.js:123:24)"
 	printf "\`abjkdsljfkds\` has been deprecated, please migrate to \`abjkdsljfkds\`\r at abjkdsljfkds(/src/node_modules/somepath:38392:35)\r at Something.SomethingElse (/src/node_modules/someotherpath.js:123:24)\r"
 
+	echo "line $i"  >&2
+	printf "\`abjkdsljfkds\` has been deprecated, please migrate to \`abjkdsljfkds\`\n at abjkdsljfkds(/src/node_modules/somepath:38392:35)\n at Something.SomethingElse (/src/node_modules/someotherpath.js:123:24)\n" >&2
+	printf "\`abjkdsljfkds\` has been deprecated, please migrate to \`abjkdsljfkds\` at abjkdsljfkds(/src/node_modules/somepath:38392:35) at Something.SomethingElse (/src/node_modules/someotherpath.js:123:24)" >&2
+	printf "\`abjkdsljfkds\` has been deprecated, please migrate to \`abjkdsljfkds\`\r at abjkdsljfkds(/src/node_modules/somepath:38392:35)\r at Something.SomethingElse (/src/node_modules/someotherpath.js:123:24)\r" >&2
+
+	cp output /dev/stdout
 	cp output /dev/stderr
 
 done
