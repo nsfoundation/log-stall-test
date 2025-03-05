7b3722bfaee7024cab7111153db90f33f05d2dc8
Pavol Marko <pavol@namespacelabs.com>
2025-03-05 11:02:54 +0100

    more output

--- long/main.go
+++ long/main.go
@@ -10,11 +10,22 @@ import (
 func main() {
 		var buf bytes.Buffer
 
-		for i := 0; i <= 256_000; i++ {
-			buf.WriteByte('a')
+		for i := 0; i <= 256; i++ {
+			for j := 0; j <= 1000; i++ {
+				buf.WriteByte('a')
+			}
+			buf.WriteByte('\n')
 		}
 
 		_, err := fmt.Fprint(os.Stderr, buf.String())
+		if err != nil {
+			fmt.Printf("failed to flush: %v", err)
+			return
+		}
+
+		fmt.Printf("flushed a %d byte string to stderr", buf.Len())
+
+		_, err = fmt.Fprint(os.Stdout, buf.String())
 		if err != nil {
 			fmt.Printf("failed to flush: %v", err)
 			return
