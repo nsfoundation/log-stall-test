db4d41861564fe9603b1c0c98ff89c4a5c221d71
Pavol Marko <pavol@namespacelabs.com>
2025-03-05 10:42:06 +0100

    more output

--- testwithdocker/tests.sh
+++ testwithdocker/tests.sh
@@ -6,8 +6,10 @@ do
 
 	echo "line $i"
 	printf "\`abjkdsljfkds\` has been deprecated, please migrate to \`abjkdsljfkds\`\n at abjkdsljfkds(/src/node_modules/somepath:38392:35)\n at Something.SomethingElse (/src/node_modules/someotherpath.js:123:24)\n"
+	printf "\`abjkdsljfkds\` has been deprecated, please migrate to \`abjkdsljfkds\` at abjkdsljfkds(/src/node_modules/somepath:38392:35) at Something.SomethingElse (/src/node_modules/someotherpath.js:123:24)"
+	printf "\`abjkdsljfkds\` has been deprecated, please migrate to \`abjkdsljfkds\`\r at abjkdsljfkds(/src/node_modules/somepath:38392:35)\r at Something.SomethingElse (/src/node_modules/someotherpath.js:123:24)\r"
 
-	cat output >&2
+	cp output /dev/stdout
 
 done
 
