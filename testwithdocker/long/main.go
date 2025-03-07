7e425a49f473164f45d51282fef855c742bc3a40
Pavol Marko <pavol@namespacelabs.com>
2025-03-07 12:51:20 +0100

    more stuff

--- testwithdocker/long/main.go
+++ testwithdocker/long/main.go
@@ -17,20 +17,53 @@ func main() {
 			allBytes = append(allBytes, byte(j))
 		}
 
+		var totalBuf []byte
+
 		for i := 0; i <= 100; i++ {
 			time.Sleep(300*time.Millisecond)
 			// long line
 			os.Stdout.Write(buf)
+			totalBuf = append(totalBuf, buf...)
 			os.Stdout.Write(buf)
+			totalBuf = append(totalBuf, buf...)
 			os.Stdout.Write(buf)
+			totalBuf = append(totalBuf, buf...)
 			os.Stdout.Write(buf)
+			totalBuf = append(totalBuf, buf...)
 			os.Stdout.Write(buf)
+			totalBuf = append(totalBuf, buf...)
 			os.Stdout.WriteString("\n")
+			totalBuf = append(totalBuf, '\n')
 			// special line
 			os.Stdout.Write(allBytes)
+			totalBuf = append(totalBuf, allBytes...)
 			os.Stdout.WriteString("\n")
+			totalBuf = append(totalBuf, '\n')
 			// short line
 			os.Stdout.Write(buf)
+			totalBuf = append(totalBuf, buf...)
 			os.Stdout.WriteString("\n")
+			totalBuf = append(totalBuf, '\n')
+
+			// long line
+			os.Stderr.Write(buf)
+			os.Stderr.Write(buf)
+			os.Stderr.Write(buf)
+			os.Stderr.Write(buf)
+			os.Stderr.Write(buf)
+			os.Stderr.WriteString("\n")
+			// special line
+			os.Stderr.Write(allBytes)
+			os.Stderr.WriteString("\n")
+			// short line
+			os.Stderr.Write(buf)
+			os.Stderr.WriteString("\n")
 		}
+
+
+		// and all at once, twice :)
+		os.Stdout.Write(totalBuf)
+		os.Stderr.Write(totalBuf)
+		os.Stdout.Write(totalBuf)
+		os.Stderr.Write(totalBuf)
 }
